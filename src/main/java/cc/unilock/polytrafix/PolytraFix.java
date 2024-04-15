a79371a43d04cca8693d6979ff840f6e29a032d7
unilock <unilock@fennet.rentals>
2024-04-15 08:51:54 -0400

    Use DamageableItemPoly instead of CustomModelDataPoly
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/polytrafix/PolytraFix.java
+++ src/main/java/cc/unilock/polytrafix/PolytraFix.java
@@ -3,13 +3,13 @@ package cc.unilock.polytrafix;
 import io.github.theepicblock.polymc.api.PolyMcEntrypoint;
 import io.github.theepicblock.polymc.api.PolyRegistry;
 import io.github.theepicblock.polymc.api.item.CustomModelDataManager;
-import io.github.theepicblock.polymc.impl.poly.item.CustomModelDataPoly;
+import io.github.theepicblock.polymc.impl.poly.item.DamageableItemPoly;
 import net.minecraft.item.Items;
 import nourl.mythicmetals.armor.MythicArmor;
 
 public class PolytraFix implements PolyMcEntrypoint {
 	@Override
 	public void registerPolys(PolyRegistry registry) {
-        registry.registerItemPoly(MythicArmor.CELESTIUM_ELYTRA, new CustomModelDataPoly(registry.getSharedValues(CustomModelDataManager.KEY), MythicArmor.CELESTIUM_ELYTRA, Items.ELYTRA));
+        registry.registerItemPoly(MythicArmor.CELESTIUM_ELYTRA, new DamageableItemPoly(registry.getSharedValues(CustomModelDataManager.KEY), MythicArmor.CELESTIUM_ELYTRA, Items.ELYTRA));
 	}
 }
